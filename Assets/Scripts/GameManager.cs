c11a8aa63f5c3a4053dbf496244b025c2c092654
Ben-Wohlford <144173314+Ben-Wohlford@users.noreply.github.com>
2024-03-15 13:50:26 -0700

    03/15/2024

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.UIElements;
 using Random = UnityEngine.Random;
@@ -9,13 +10,12 @@ using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
 {
+    private GameObject Canvas;
     public static GameManager gm;
     public List<Card> deck = new List<Card>();
-    public List<Card> player_deck = new List<Card>();
-    public List<Card> ai_deck = new List<Card>();
     public List<Card> playerHand = new List<Card>();
+    private int playerSum;
     public List<Card> ai_hand = new List<Card>();
-    public List<Card> discard_pile = new List<Card>();
     private void Awake()
     {
         if (gm != null && gm != this)
@@ -31,14 +31,30 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Canvas = GameObject.Find("Canvas");
         int playerDraw = 2;
         for (int x = 0; x < playerDraw; x++)
         {
-            int drawCard = Random.Range(0, deck.Count-1);
-            Debug.Log(deck[drawCard].value);
+            int drawCard = Random.Range(0, deck.Count);
+            Card current = Instantiate(deck[drawCard], new Vector3(200+(x*150), 100, 0), quaternion.identity);
+            current.transform.SetParent(Canvas.transform);
+            Debug.Log("Card: " + deck[drawCard].value);
             playerHand.Add(deck[drawCard]);
             deck.Remove(deck[drawCard]);
         }
+        for (int x = 0; x < playerHand.Count; x++)
+        {
+            playerSum += playerHand[x].value;
+        }
+        for (int x = 0; x < playerHand.Count; x++)
+        {
+            if (playerHand[x].value == 1 && playerSum < 12)
+            {
+                playerSum += 10;
+                break;
+            }
+        }
+        Debug.Log("Sum: " + playerSum);
     }
 
     // Update is called once per frame
